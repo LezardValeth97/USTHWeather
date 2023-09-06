f2198b07cbce4345448432e16be0f38fab06234a
LezardValeth97 <themasterofwind1997@gmail.com>
2023-09-06 10:34:19 +0700

    Prac 5 add blue forecast area

--- app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
@@ -22,21 +22,22 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        TextView day = new TextView(getContext());
-        day.setText("Thursday");
+//        TextView day = new TextView(getContext());
+//        day.setText("Thursday");
+//
+//        ImageView img = new ImageView(getContext());
+//        img.setImageResource(R.drawable.ic_action_name);
+//
+//        View v = new View(getContext());
+//        v.setBackgroundColor(0x20FF0000);
+//
+//        LinearLayout view = new LinearLayout(getContext());
+//        view.setOrientation(LinearLayout.VERTICAL);
+//        view.addView(day);
+//        view.addView(img);
+//        view.addView(v);
+//        return view;
+        return inflater.inflate(R.layout.fragment_forecast, container, false);
 
-        ImageView img = new ImageView(getContext());
-        img.setImageResource(R.drawable.ic_action_name);
-
-        View v = new View(getContext());
-        v.setBackgroundColor(0x20FF0000);
-
-        LinearLayout view = new LinearLayout(getContext());
-        view.setOrientation(LinearLayout.VERTICAL);
-        view.addView(day);
-        view.addView(img);
-        view.addView(v);
-//        return inflater.inflate(R.layout.fragment_forecast, container, false);
-        return view;
     }
 }
---
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherFragment.java
@@ -0,0 +1,33 @@
+package vn.edu.usth.usthweather;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WeatherFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WeatherFragment extends Fragment {
+
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+}
