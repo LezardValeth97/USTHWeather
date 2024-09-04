d75ff5a0342f4ce460e506d708fabdb9f98023f0
LezardValeth97 <themasterofwind1997@gmail.com>
2024-09-04 16:22:20 +0700

    Prac 7: - Created WeatherAndForecastFragment - Finished swiping things

--- app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -1,6 +1,13 @@
 package vn.edu.usth.usthweather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 import android.util.Log;
 
@@ -12,11 +19,18 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+/*
         // Create a new Fragment to be placed in the activity layout
-//        ForecastFragment f = new ForecastFragment();
-//        // Add the fragment to the 'container' FrameLayout
-//        getSupportFragmentManager().beginTransaction().add(
-//                R.id.activity_weather, f).commit();
+        ForecastFragment f = new ForecastFragment();
+        // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.activity_weather, f).commit();
+*/
+
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
 
         Log.i(TAG, "===== App Created =====");
     }
@@ -51,6 +65,32 @@ public class WeatherActivity extends AppCompatActivity {
         Log.i(TAG, "##### App Destroyed #####");
     }
 
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+        private final String[] titles = new String[]{"Hanoi", "Paris", "Toulouse"};
 
+        public HomeFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;          // number of pages for a ViewPager
+//            return (titles.length);   // number of pages for a ViewPager
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int page){
+            // returns an instance of Fragment corresponding to the specified page
+            return new WeatherAndForecastFragment();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int page) {
+            // returns a tab title corresponding to the specified page
+            return titles[page];
+        }
+    }
 
 }
---
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherAndForecastFragment.java
@@ -0,0 +1,30 @@
+package vn.edu.usth.usthweather;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WeatherAndForecastFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WeatherAndForecastFragment extends Fragment {
+
+
+    public WeatherAndForecastFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather_and_forecast, container, false);
+    }
+}
