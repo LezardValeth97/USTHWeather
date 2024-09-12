87a0a8a66794036f2ba6c6a54655bd22fde66f75
LezardValeth97 <themasterofwind1997@gmail.com>
2024-09-12 10:25:36 +0700

    Prac 12: Add ToolBar and 2 icons (refresh and settings)

--- app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -8,23 +8,28 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
     private static final String TAG = "WeatherActivity";
-    MediaPlayer mp;
+//    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mp = MediaPlayer.create(this, R.raw.farout);
-//        mp.setVolume(70,70);
-        mp.start();
+//        mp = MediaPlayer.create(this, R.raw.farout);
+//        mp.start();
 /*
         // Create a new Fragment to be placed in the activity layout
         ForecastFragment f = new ForecastFragment();
@@ -40,22 +45,18 @@ public class WeatherActivity extends AppCompatActivity {
         TabLayout tablayout = findViewById(R.id.tablayout);
         tablayout.setupWithViewPager(pager);
 
-
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         Log.i(TAG, "===== App Created =====");
-
     }
+
+
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "***** App Started *****");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "----- App Resumed -----");
     }
 
     @Override
@@ -65,10 +66,16 @@ public class WeatherActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "----- App Resumed -----");
+    }
+
+    @Override
     protected void onStop() {
         super.onStop();
-        mp.stop();
-        mp.release();
+//        mp.stop();
+//        mp.release();
         Log.i(TAG, "+++++ App Stopped +++++");
     }
 
@@ -77,6 +84,41 @@ public class WeatherActivity extends AppCompatActivity {
         super.onDestroy();
         Log.i(TAG, "##### App Destroyed #####");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_weather, menu);
+        return true;
+    }
+
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                AsyncTask<Void, Integer, Void> task = new AsyncTask<Void, Integer, Void>() {
+                    @Override
+                    protected Void doInBackground(Void... voids) {
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void voids){
+                        Toast.makeText(getApplicationContext(),"Updated",Toast.LENGTH_LONG).show();
+                    }
+                };
+                task.execute();
+                return true;
+            case R.id.action_settings:
+                Intent myIntent = new Intent(this, PrefActivity.class);
+                this.startActivity(myIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
         private final int PAGE_COUNT = 3;
